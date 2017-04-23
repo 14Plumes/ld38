d5ef1435e8808f626670bf14f4f71b6449b9f139
Soreine <soreine.plume@gmail.com>
2017-04-23 10:22:42 +0200

    Add ship

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -1,8 +1,12 @@
 const game = {};
 
 game.create = function create() {
-    const logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'planet');
-    logo.anchor.setTo(0.5, 0.5);
+    // Planet
+    const planet = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'planet');
+    planet.anchor.setTo(0.5, 0.5);
+    // Ship
+    const ship = this.game.add.sprite(this.game.world.centerX, planet.top, 'ship');
+    ship.anchor.setTo(0.5, 1);
 };
 
 module.exports = game;
--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -2,6 +2,7 @@ const preloader = {};
 
 preloader.preload = function preload() {
     this.game.load.image('planet', 'images/planet.png');
+    this.game.load.image('ship', 'images/ship.png');
 };
 
 preloader.create = function create() {
